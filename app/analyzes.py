6cf89832a993d855336ccb2e9d64b74200c8c2f0
Luzia-Silva <aluziagabriela@gmail.com>
2023-06-04 17:05:35 -0300

    Analises e conexões com DB

---
+++ app/analyzes.py
@@ -0,0 +1,48 @@
+import cv2
+import cv2
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+def DegreesOfBurns():
+  matplotlib.use('TkAgg') 
+  image = cv2.imread("images/BLOG703334.jpg") # Carregar imagem 
+  image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # Colocando cor da imagem
+  image_array = np.array(image) # Converta a imagem para um array NumPy
+
+  # Defina a faixa de valores de canal para a cor vermelha
+  low = np.array([0, 0, 0], dtype=np.uint8)  # Limite inferior (preto)
+  high = np.array([50, 50, 255], dtype=np.uint8)  # Limite superior (vermelho)
+
+  # Encontre os pixels que estão na faixa de cor vermelha
+  mask = cv2.inRange(image_array, low, high)
+
+  # Conte o número de pixels com a cor desejada
+  pixels_color_red = np.count_nonzero(mask)
+
+  # Calcule a porcentagem total
+  porcentage = (pixels_color_red / 100)
+  return porcentage
+  
+def AnalyzesTheBurns(burn_value, burns_values): 
+      if len(burns_values) > 0:
+        values = np.array(burns_values, dtype=float)
+        bigger = np.max(values)
+        smaller = np.min(values)
+        median = np.median(values)
+        if str(burn_value) <= str(smaller):
+            print(" \n 1º grau: atinge a epiderme (camada superficial da pele).")
+            print("\n Dados encontrados: \n MÉDIA:" + str(median) + " \n MAIOR:" + str(bigger) + "\n MENOR:" + str(smaller) + "\n\n")
+        elif str(self.burn_value) <= str(bigger):
+            print("\n 2º grau: atinge a epiderme e parte da derme (2ª camada da pele). \n Há presença de bolhas, e uma vermelhidão mais intensa e a dor é acentuada;")
+            print("\n Dados encontrados: \n MÉDIA:" + str(median) + " \n MAIOR:" + str(bigger) + "\n MENOR:" + str(smaller) + "\n\n")
+        else:
+            print("\n Nosso sistema não é capaz de realizar essa análises ápos 2º de grau de queimadura.")
+      else: 
+          print("\n\n Essa é seu primeiro dado em nossa base, faça carregamento de mais dados para obter análises de MÉDIA, MAIOR E MENOR valores!\n")
+    
+
+
+
+
+
--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
-
+import numpy as np
 import os.path
-
+import analyzes
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -17,34 +17,45 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 def main():
     creds = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists('auth/token.json'):
+        creds = Credentials.from_authorized_user_file('auth/token.json', SCOPES)
 
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                'auth/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
             
-        with open('token.json', 'w') as token:
+        with open('auth/token.json', 'w') as token:
             token.write(creds.to_json())
 
     try:
         service = build('sheets', 'v4', credentials=creds)
         sheet = service.spreadsheets()
-        result = sheet.values().get(spreadsheetId=os.environ.get("SAMPLE_SPREADSHEET_ID"),
-                                    range=os.environ.get("SAMPLE_RANGE_NAME")).execute()
-        values = result.get('values', [])
-
-        if not values:
-            print('No data found.')
+        burns = analyzes.DegreesOfBurns()
+        if not burns:
+            print("Sorry, i'dont identification your data burns.")
         else:
-            print(values)
-            for row in values:
-                # Print columns A the colors-porcentage
-                print(row[0])
+            get_burns = sheet.values().get(spreadsheetId=os.environ.get("SAMPLE_SPREADSHEET_ID"),
+                                    range='database!A1:A51').execute()
+            values = get_burns.get('values', [])
+            analysesTheDegree = analyzes.AnalyzesTheBurns(burn_value=burns, burns_values=values)
+            array = np.array(values)
+            is_present = np.isin(array, str(burns)) 
+            exists_value = np.any(is_present)
+            if exists_value:
+                print("Não vamos armazenar em nossa base esse dado, porque ele existe, porém não irá atrapalhar em nossas análises!")
+            else:    
+                body_burns =  {
+                        'values' : [[ str(burns)]]
+                    }
+                sheet.values().append(spreadsheetId=os.environ.get("SAMPLE_SPREADSHEET_ID"),
+                                            range='database!A1', valueInputOption="USER_ENTERED", insertDataOption="INSERT_ROWS", body=body_burns).execute()
+                    
+            
+
     except HttpError as err:
         print(err)
 
